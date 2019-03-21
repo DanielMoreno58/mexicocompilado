d59f0012a988f302665edc134efc76fedefca885
Daniel Moreno <daniel10697@hotmail.com>
2019-03-21 13:57:27 -0600

    Add Anwser model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_181430) do
+ActiveRecord::Schema.define(version: 2019_03_21_195313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "answer_description"
+    t.string "author"
+    t.integer "answer_chosen"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "question_name"
