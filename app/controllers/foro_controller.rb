62461b18a5206d91fab807d3a8fa84325db6053a
Daniel Moreno <daniel10697@hotmail.com>
2019-03-21 04:01:33 -0600

    Add a view of question form

--- app/controllers/foro_controller.rb
+++ app/controllers/foro_controller.rb
@@ -1,6 +1,25 @@
 class ForoController < ApplicationController
 
   def index
+    @foro = Question.new
   end
 
+  def new
+    @foro = Question.new
+  end
+
+  # def create
+  #   @foro = current_user.questions.new(question_params)
+  #   if @foro.save
+  #     redirect_to @foro
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+  # private
+
+  # def article_params
+  #   params.require(:question).permit(:question_name, :status)
+  # end
 end
